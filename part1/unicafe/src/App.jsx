fc0cd2bd46627059c2ef7f15c748c4cbdb90888d
mauricio <jourdanmauricio@gmail.com>
2025-05-06 08:57:54 -0300

    Part1c Agrego ejercicio 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,15 @@
 import { useState } from 'react'
 
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 const Statistics = ({good, neutral, bad}) => {
 
   if (good + neutral + bad === 0) {
@@ -7,14 +17,16 @@ const Statistics = ({good, neutral, bad}) => {
   } 
 
   return (
-  <>
-    <p>good {good}</p>
-    <p>nutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {good + neutral + bad}</p>
-    <p>average {(good - bad) / (good + neutral + bad)}</p>
-    <p>positive {(good * 100) / (good + neutral + bad)} %</p>
-  </>
+  <table>
+    <tbody>
+      <StatisticLine text='good' value={good}/>
+      <StatisticLine text='neutral' value={neutral}/> 
+      <StatisticLine text='bad' value={bad}/>
+      <StatisticLine text='all' value={good + neutral + bad}/> 
+      <StatisticLine text='average' value={(good - bad) / (good + neutral + bad)} /> 
+      <StatisticLine text='positive' value={(good * 100) / (good + neutral + bad) + ' %'} /> 
+    </tbody>
+  </table>
   )
 }
 
