aa3c4c0d706af48c475d11b0107d13962048b30a
mauricio <jourdanmauricio@gmail.com>
2025-05-08 06:54:13 -0300

    Part2 Agrego ejercicio 2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -22,7 +22,7 @@ const Content = ({parts}) => {
 
 const Total = ({parts}) => {
   return (
-    <p><strong>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises + parts[3].exercises}</strong></p>
+    <p><strong>Number of exercises {parts.reduce((acc, part) => acc + part.exercises, 0)}</strong></p>
   )
 }
 
